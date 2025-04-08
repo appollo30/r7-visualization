2c157ac39cf4524a33099e1863a581c1020b23ec
appollo30 <leo.lasnier@me.com>
2025-04-08 10:37:48 +0200

    ajout du test de modele

--- pages/3_🔌_Tester_le_modèle.py
+++ pages/3_🔌_Tester_le_modèle.py
@@ -1,23 +1,32 @@
 from src.file_uploader import handle_file_uploader
 from src.walking_recording import WalkingRecording
 from src.plot_factory import PlotFactory
-from src.ml_utils import load_model, predict_from_wr
-import pandas as pd
+from src.ml_utils import predict_from_wr, show_container, get_inference_session
 import streamlit as st
-import keras
 
 def main():
+    st.set_page_config(
+        page_title="Tester le modèle",
+        layout="wide",
+    )
+    
     st.title("Tester le modèle")
+    
+    inference_session = get_inference_session("models/cnn_3layers_79.30_20250408092059/model_250.onnx")
     
     walking_recordings = handle_file_uploader()
     
-    model = load_model()
+    col1, col2 = st.columns(2)
     
-    if len(walking_recordings) > 0:
-        with st.spinner("Prédiction en cours"):
-            prediction = predict_from_wr(walking_recordings[0],model)
-            
-        st.write(prediction)
+    for i, wr in enumerate(walking_recordings):
+        predicted_names = predict_from_wr(wr, inference_session)
+        if i % 2 == 0:
+            with col1:
+                show_container(wr, predicted_names)
+        else:
+            with col2:
+                show_container(wr, predicted_names)
+        
     
 
 main()
--- src/ml_utils.py
+++ src/ml_utils.py
@@ -1,21 +1,21 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import keras
 from src.walking_recording import WalkingRecording
 from collections import Counter
 from typing import List
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import onnxruntime as ort
 
-names = ['Antoine', 'Corentin', 'Felix', 'Leo', 'Vladislav', 'Matthieu', 'Serge']
+names = sorted(['Antoine', 'Corentin', 'Felix', 'Leo', 'Vladislav', 'Matthieu', 'Serge'])
+color_scheme = {name : color for name, color in zip(names,px.colors.qualitative.Plotly)}
 
-@st.cache_data
-def load_model(verbose : bool = True) -> keras.Model :
-    if verbose:
-        with st.spinner("Chargement du modèle...", show_time=True):
-            model = keras.saving.load_model("models/cnn_3layers_75.46_20250324215033/model_250.keras")
-    else :
-        model = keras.saving.load_model("models/cnn_3layers_75.46_20250324215033/model_250.keras")
-    return model
+@st.cache_resource
+def get_inference_session(model_path : str) -> ort.InferenceSession:
+    session = ort.InferenceSession(model_path)
+    return session
 
 def create_sequences(data, sequence_size=250, hop_length=1) -> np.ndarray:
     sequences = []
@@ -27,19 +27,78 @@ def create_sequences(data, sequence_size=250, hop_length=1) -> np.ndarray:
     return np.reshape(sequences,(sequences.shape[0],sequences.shape[1],1))
 
 def get_counter(predicted_names : List[str]) -> Counter:
-    return sorted(Counter(predicted_names))
+    return Counter(predicted_names).most_common()
 
-@st.cache_data
-def predict_from_wr(walking_recording : WalkingRecording, model : keras.Model, verbose : bool = True):
-    sequence_size = model.layers[0].input.shape[1]
+def predict_from_wr(walking_recording : WalkingRecording, inference_session, verbose : bool = True):
+    sequence_size = inference_session.get_inputs()[0].shape[1]
     
     df = walking_recording.df
     data = df["acceleration (g)"].values
     sequences = create_sequences(data, sequence_size) 
-    prediction = model.predict(sequences)
+    prediction = inference_session.run(None, {inference_session.get_inputs()[0].name: sequences.astype(np.float32)})[0]
     predicted_indices = np.argmax(prediction,axis=1)
     predicted_names = [names[idx] for idx in predicted_indices]
     return predicted_names
 
-def get_predictions_plot(walking_recording : WalkingRecording, predicted_names : List[str], sequence_size):
-    pass
+def to_gantt(predicted_names):
+    output_data = []
+    start = 0
+    finish = 1
+    state = predicted_names[0]
+    for i, name in enumerate(predicted_names):
+        finish = i
+        if name != state:
+            output_data.append({'prediction' : state, "start" : start, "finish" : finish})
+            start = finish
+            state = name
+    output_data.append({'prediction' : state, "start" : start, "finish" : finish})
+    output_df = pd.DataFrame(output_data)
+    return output_df
+
+def show_container(walking_recording : WalkingRecording, predicted_names : List[str]):
+    c = get_counter(predicted_names)
+    n = len(predicted_names)
+    with st.container(border=True,):
+        st.caption(f"{walking_recording.name}/{walking_recording.file_name}")
+        st.markdown(f"### Utilisateur prédit : **{c[0][0]}**")
+        st.plotly_chart(make_area_plot(walking_recording.df, predicted_names), use_container_width=True)
+        st.markdown("### Détail des prédictions :")
+        for name, count in c:
+            st.markdown(f"{name} : {count/n:.2%}")
+        
+
+def make_area_plot(df,predicted_names):
+    gantt = to_gantt(predicted_names)
+    nameset = set()
+    fig = go.Figure()
+    for i, row in gantt.iterrows():
+        start = row["start"]
+        finish = row["finish"]
+        name = row["prediction"]
+        df_subset = df[start:finish+1]
+        trace = go.Scatter(
+            x = df_subset["time (s)"],
+            y = df_subset["acceleration (g)"],
+            mode="lines",
+            fill='tozeroy',
+            line_color = color_scheme[name],
+            legendgroup=name,
+            name=name,  
+        )
+        if name in nameset:
+            trace.update(showlegend=False)
+        else:
+            nameset.add(name)
+        fig.add_trace(trace)
+    
+    fig.update_layout(
+        title="Prédicitons de l'utilisateur",
+        xaxis_title="Time (s)",
+        yaxis_title="Acceleration (g)",
+        showlegend=True,
+        legend_title_text='Prédictions',
+    )
+    
+    return fig
+
+        
